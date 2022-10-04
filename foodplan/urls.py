a1d579c9aa1e3b213a5e11c112d6b0c3191c4218
EgoisTa <97385729+EgoisTa-Git@users.noreply.github.com>
2022-10-04 23:02:12 +0300

    Update urls.py

--- foodplan/urls.py
+++ foodplan/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
+from food_plan import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
