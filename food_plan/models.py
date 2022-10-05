7bb32baf08f89ac8b32deebab96dc252e6846bd5
Ivan Kumeyko <kumeyko@hotmail.com>
2022-10-05 19:52:41 +0200

    first recipe model created

--- food_plan/models.py
+++ food_plan/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Recipe(models.Model):
+    recipe_name = models.CharField(
+        verbose_name='Название рецепта',
+        max_length=200
+    )
+
+    added_date = models.DateTimeField(
+        verbose_name='Дата добавления',
+        default=timezone.now
+    )
+    modified_date = models.DateTimeField(
+        verbose_name='Дата изменения',
+        null=True
+    )
+
+    def __str__(self) -> str:
+        return f"{self.recipe_name}"
+
+    class Meta:
+        verbose_name = 'Recipe'
+        verbose_name_plural = 'Recipes'
