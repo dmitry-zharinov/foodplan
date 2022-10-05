3d9cdf7c277a0ebfdc738c5680bcfaea24982038
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-10-05 14:15:51 +0300

    Add login_required decorator to profile view

--- food_plan/views.py
+++ food_plan/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     context = {
     }
     return render(request, 'index.html', context)
 
-
+@login_required
 def profile(request):
     context = {
     }
