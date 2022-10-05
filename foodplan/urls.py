80511c45fd7e4afedc004b37c69aca5ec9a7c6b4
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-10-05 11:16:05 +0300

    Add login and registration forms

--- foodplan/urls.py
+++ foodplan/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -23,5 +23,8 @@ from food_plan import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+
+    path('auth/', include('django.contrib.auth.urls')),
+    path('auth/', include('accounts.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
