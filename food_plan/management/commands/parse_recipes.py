3100dbb77bc5f00744c941371a44c54a5cf809da
Ivan Kumeyko <kumeyko@hotmail.com>
2022-10-07 00:41:46 +0200

    parser finalized

--- food_plan/management/commands/parse_recipes.py
+++ food_plan/management/commands/parse_recipes.py
@@ -1,21 +1,18 @@
-from django.core.management.base import BaseCommand, CommandError
-from polls.models import Question as Poll
+from django.core.management.base import BaseCommand
+from food_plan.models import Recipe, Ingredient
 import requests
 from bs4 import BeautifulSoup
 import time
+import urllib.request
 
 
 class Command(BaseCommand):
     help = 'Parse recipe'
 
     def handle(self, *args, **options):
-        pass
-
-    def get_recipes(self):
-        recipes_collection = []
         headers = {'User-Agent': 'Chrome/51.0.2704.64 Safari/537.36'}
         urls = [
-            f'https://www.edimdoma.ru/retsepty?page={n}' for n in range(1, 5)]
+            f'https://www.edimdoma.ru/retsepty?page={n}' for n in range(4, 5)]
         for url in urls:
             time.sleep(5)
             self.stdout.write(self.style.WARNING("Going thru pages"))
@@ -23,31 +20,51 @@ class Command(BaseCommand):
             bs = BeautifulSoup(res.text, 'html.parser')
             recipes_urls = bs.find_all("div", {"class": "card__description"})
             for recipe_url in recipes_urls:
-
                 time.sleep(5)
                 recipe_href = recipe_url.find("a")["href"]
                 recipe_url = f'https://www.edimdoma.ru{recipe_href}'
-                self.stdout.write(self.style.WARNING("Going thru recipes"))
+                self.stdout.write(f"Going thru {recipe_url}")
                 recipe_response = requests.get(recipe_url, headers=headers)
                 recipe_bs = BeautifulSoup(recipe_response.text, 'html.parser')
-
+                self.stdout.write("Scraped")
                 recipe_portions = recipe_bs.find(
                     "input", {"class": "field__input"})['value']
                 recipe_name = recipe_bs.find(
                     "h1", {"class": "recipe-header__name"}).text
-                recipe_instruction = recipe_bs.find(
-                    "div", {"class": "recipe_description"}).text
+
+                recipe_instruction = recipe_bs.find_all(
+                    "div", {"class": "plain-text recipe_step_text"})
+
+                recipe_instruction = ' '.join(
+                    [step.text for step in recipe_instruction])
+                try:
+                    recipe_description = recipe_bs.find(
+                        "div", {"class": "recipe_description"}).text
+                except AttributeError:
+                    recipe_description = 'Нет описания'
+                try:
+                    recipe_image_url = recipe_bs.find(
+                        "div", {"class": "content-media"}).find("img")['src']
+                    recipe_image = recipe_image_url.split('/')[-1]
+                    recipe_image = recipe_image.split('?')[0]
+                    urllib.request.urlretrieve(
+                        recipe_image_url, f'static/img/recipes/{recipe_image}')
+                    self.stdout.write("Image saved!")
+                except TypeError:
+                    recipe_image = 'default.jpg'
                 recipe_ingridients = recipe_bs.find(
                     "div", {"id": "recipe_ingredients_block"}).find_all(
                     "table", {"class": "definition-list-table"})
+                recipe = Recipe(
+                    title=recipe_name,
+                    description=recipe_description,
+                    instructions=recipe_instruction,
+                    image=recipe_image,
+                    portions=recipe_portions
+                )
+                recipe.save()
+                self.stdout.write("Recipe saved.")
 
-                full_recipe = {
-                    'recipe_name': recipe_name,
-                    'recipe_portions': recipe_portions,
-                    'recipe_instruction': recipe_instruction
-                }
-
-                ingridient = []
                 for recipe_ingridient in recipe_ingridients:
                     ingridient_name = recipe_ingridient.find(
                         "input",
@@ -61,12 +78,13 @@ class Command(BaseCommand):
                         "input",
                         {"class": "checkbox__input recipe_ingredient_checkbox"}
                     )['data-unit-title']
-                    ingridient.append({
-                        'ingridient_name': ingridient_name,
-                        'ingridient_amount': ingridient_amount,
-                        'ingridient_unit': ingridient_unit}
-                    )
 
-                full_recipe['ingridients'] = ingridient
-                recipes_collection.append(full_recipe)
-        return recipes_collection
+                    ingredient = Ingredient(
+                        amount=ingridient_amount,
+                        title=ingridient_name,
+                        unit=ingridient_unit,
+                        recipe=recipe
+
+                    )
+                    ingredient.save()
+                    self.stdout.write("Ingredient saved.")
--- food_plan/models.py
+++ food_plan/models.py
@@ -1,79 +1,100 @@
+from email.policy import default
+from django.contrib.auth.models import User
 from django.db import models
+
+PERIOD_CHOICES = [
+    ('3', '3 мес.'),
+    ('12', '12 мес.'),
+]
+
+RECIPE_TYPE = [
+    ('breakfast', 'завтрак'),
+    ('lunch', 'обед'),
+    ('supper', 'ужин'),
+    ('dessert', 'десерт'),
+]
 
 
 class Menu(models.Model):
-    name = models.CharField(
-        'название',
-        max_length=50
+    client = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        null=True,
     )
-    description = models.TextField(
-        'описание',
-        max_length=200,
+    period = models.CharField(
+        'Срок подписки',
+        max_length=10,
+        choices=PERIOD_CHOICES,
+        default='3',
+    )
+    calories_per_day = models.IntegerField(
+        'Калорий в день (всего)',
+        default=0,
+    )
+    with_breakfasts = models.BooleanField(
+        'Завтраки',
+        default=True,
+    )
+    with_lunches = models.BooleanField(
+        'Обеды',
+        default=True,
+    )
+    with_suppers = models.BooleanField(
+        'Ужины',
+        default=True,
+    )
+    with_desserts = models.BooleanField(
+        'Десерты',
+        default=True,
+    )
+    persons = models.IntegerField(
+        'Количество персон',
+        default=1,
+    )
+    allergens = models.ManyToManyField(
+        'Allergen',
+        verbose_name='Исключить аллергены',
+        related_name='in_menus',
         blank=True,
     )
 
     class Meta:
-        verbose_name = 'меню'
-        verbose_name_plural = 'меню'
+        verbose_name = 'Меню'
+        verbose_name_plural = 'Меню'
 
     def __str__(self):
-        return self.name
-
-
-class Allergen(models.Model):
-    allergen_name = models.CharField('Название аллергена', max_length=200)
-
-    class Meta:
-        verbose_name = 'Аллерген'
-        verbose_name_plural = 'Аллергены'
-
-    def __str__(self):
-        return self.allergen_name
-
-
-class Unit(models.Model):
-    name = models.CharField('Единица измерения продукта', max_length=50)
-
-    class Meta:
-        verbose_name = 'Единица измерения продукта'
-        verbose_name_plural = 'Единицы измерения'
-
-    def __str__(self):
-        return self.name
-
-
-class Ingredient(models.Model):
-    name = models.CharField('Название ингредиента', max_length=200)
-    unit = models.ForeignKey(
-        Unit,
-        on_delete=models.SET_NULL,
-        verbose_name='Единица измерения',
-        null=True)
-
-    allergen = models.ForeignKey(
-        Allergen,
-        on_delete=models.SET_NULL,
-        verbose_name='Аллерген',
-        null=True)
-
-    class Meta:
-        verbose_name = 'Ингредиент'
-        verbose_name_plural = 'Ингредиенты'
-
-    def __str__(self):
-        return self.name
+        return f'Меню для {self.client}'
 
 
 class Recipe(models.Model):
-    title = models.CharField('Название рецепта', max_length=200)
+    title = models.CharField(
+        'Название рецепта',
+        max_length=200,
+    )
     description = models.TextField(
+        'Краткое описание',
         blank=True,
-        verbose_name='Краткое описание')
+    )
     instructions = models.TextField(
-        verbose_name='Инструкция приготовления')
-    ingredients = models.ManyToManyField(
-        Ingredient,
-        verbose_name='Ингредиенты в рецепте')
+        'Инструкция приготовления',
+        blank=True,
+    )
+    type = models.CharField(
+        'Тип блюда',
+        max_length=10,
+        choices=RECIPE_TYPE,
+        default='завтрак',
+    )
+    image = models.ImageField(
+        'Картинка',
+        blank=True,
+    )
+
+
+    portions = models.IntegerField(
+        'Порция',
+        default=1,
+    )
 
     class Meta:
         verbose_name = 'Рецепт'
@@ -81,3 +102,73 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Ingredient(models.Model):
+    amount = models.DecimalField(
+        'Количество',
+        max_digits=6,
+        decimal_places=2,
+        default=1,
+    )
+
+    title = models.CharField(
+        'Название ингредиента',
+        max_length=120,
+        db_index=True,
+        default='ингредиент'
+    )
+
+    unit = models.CharField(
+        'Измерение ингредиента',
+        max_length=120,
+        default='1 ложка'
+    )
+
+    price = models.DecimalField(
+        'Цена за штуку',
+        max_digits=6,
+        decimal_places=2,
+        null=True,
+    )
+
+    allergen = models.ForeignKey(
+        'Allergen',
+        on_delete=models.SET_NULL,
+        verbose_name='Название аллергена',
+        related_name='products',
+        null=True,
+        blank=True,
+    )
+
+    recipe = models.ForeignKey(
+        'Recipe',
+        on_delete=models.CASCADE,
+        verbose_name='Рецепт',
+        related_name='ingredients',
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return f"{self.product}, {self.amount}"
+
+
+class Allergen(models.Model):
+    name = models.CharField(
+        'Название аллергена',
+        max_length=120,
+        db_index=True,
+        default='',
+    )
+
+    class Meta:
+        verbose_name = 'Аллерген'
+        verbose_name_plural = 'Аллергены'
+
+    def __str__(self):
+        return self.name
